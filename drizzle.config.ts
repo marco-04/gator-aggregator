ba2d6d5ba2186d3701537c315d11276c60429a40
marcosti <marcosti096@duck.com>
2026-02-14 14:16:26 +0100

    update db code structure

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { Config as GatorConfig } from "src/config.ts";
 
 export default defineConfig({
   schema: "db/schema.ts",
-  out: "src/db",
+  out: "src/db/drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: (new GatorConfig(GatorConfig.getDefaultConfigPath())).dbUrl,
--- src/state.ts
+++ src/state.ts
@@ -1,5 +1,25 @@
 import { Config } from "./config.js";
 
+import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
+
+import * as schema from "./db/schema.js";
+
 export interface State {
   cfg: Config,
+  db: ReturnType<typeof drizzle>
+}
+
+export function newState(cfg: Config): State {
+  const db = initDB(cfg);
+
+  return {
+    cfg,
+    db
+  };
+}
+
+function initDB(cfg: Config): ReturnType<typeof drizzle> {
+  const conn = postgres(cfg.dbUrl);
+  return drizzle(conn, { schema });
 }

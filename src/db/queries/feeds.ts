6148c17c1804cefa4a8c7d6e00f10fa57fb28784
marcosti <marcosti096@duck.com>
2026-02-15 19:05:50 +0100

    feed fetch updates

--- src/db/queries/feeds.ts
+++ src/db/queries/feeds.ts
@@ -1,6 +1,6 @@
-import { eq } from "drizzle-orm";
+import { asc, eq, sql } from "drizzle-orm";
 import { State } from "../../state.js";
-import { feeds, users } from "../schema.js";
+import { Feed, feeds, users } from "../schema.js";
 
 export async function createFeed(state: State, userID: string, name: string, feedURL: string) {
   const [result] = await state.db.insert(feeds).values({
@@ -19,5 +19,14 @@ export async function getFeed(state: State, feedURL: string) {
 export async function listFeeds(state: State) {
   const result = await state.db.select().from(feeds).leftJoin(users, eq(users.id, feeds.userId));
   return result;
+}
+
+export async function markFeedFetched(state: State, feedID: string) {
+  await state.db.update(feeds).set({ lastFetchedAt: new Date() }).where(eq(feeds.id, feedID));
+}
+
+export async function getNextToFetch(state: State) {
+  const [result] = await state.db.execute(sql`SELECT * FROM ${feeds} ORDER BY ${feeds.lastFetchedAt} ASC NULLS FIRST LIMIT 1`);
+  return result as Feed;
 }
 

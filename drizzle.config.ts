962430c68e280ec5d5623411f6b206291f638c3b
marcosti <marcosti096@duck.com>
2026-02-15 13:24:45 +0100

    update drizzle gen folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { Config as GatorConfig } from "src/config.ts";
 
 export default defineConfig({
   schema: "src/db/schema.ts",
-  out: "src/db/drizzle",
+  out: "drizzle",
   dialect: "postgresql",
   dbCredentials: {
     url: (new GatorConfig(GatorConfig.getDefaultConfigPath())).dbUrl,
